674bf09c7d338d5356db775ba07d30efa82b9c31
yeonee911 <128215774+yeonee911@users.noreply.github.com>
2024-01-11 19:22:48 +0900

    login error

--- src/apis/mypage.js
+++
@@ -1,8 +0,0 @@
-import { getAuthAxios } from "./authAxios";
-
-export const getMypage = async () => {
-  const access = localStorage.getItem("access");
-  const authAxios = getAuthAxios(access);
-  const result = await authAxios.get("/snowflakes");
-  return result.data;
-};
