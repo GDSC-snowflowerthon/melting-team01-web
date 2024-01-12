04cc97aceeb2ada498f3a4f3476ddbdfdd00e775
D-H1030 <hwa1328@gmail.com>
2024-01-12 23:03:29 +0900

    again update

--- src/pages/January.js
+++ src/pages/January.js
@@ -121,7 +121,6 @@ function Plan() {
   return (
     <div>
       <h2>Plan : {loading ? "Loading..." : content.plan}</h2>{" "}
-      {/*계획도 서버에서 받아온 걸 띄워야 한다..*/}
       <CheckboxSelection
         grade={grade}
         onCheckboxChange={handleCheckboxChange}
